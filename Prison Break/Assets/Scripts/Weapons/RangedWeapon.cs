a648f45c6a5c2f0ae3cce4f6df2b3b2ffe9c8635
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-06-14 21:43:48 +1200

    Minor stat adjustments

--- Prison Break/Assets/Scripts/Weapons/RangedWeapon.cs
+++ Prison Break/Assets/Scripts/Weapons/RangedWeapon.cs
@@ -15,9 +15,10 @@ public class RangedWeapon : Weapon
 
     public override void Attack(Vector3 start, Quaternion rotation, Vector3 direction, string ignoreLayer, Animator animator)
     {
+        Vector3 actualStart = start + direction * 1.5f;
         OnAttack(this);
         animator.SetTrigger("onAttack");
-        GameObject newBullet = Instantiate(bulletPrefab, start, rotation);
+        GameObject newBullet = Instantiate(bulletPrefab, actualStart, rotation);
         newBullet.GetComponent<BulletLogic>().ignoreLayer = ignoreLayer;
         newBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
     }
