2cee739efc31ed202931af75661ba166c2eae29f
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-06-20 09:08:26 +1200

    Minor updates

--- Prison Break/Assets/Scripts/Player/PlayerDeath.cs
+++ Prison Break/Assets/Scripts/Player/PlayerDeath.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class PlayerDeath : MonoBehaviour, IKillable
 {
     [SerializeField] GameObject deadPrefab;
+    [SerializeField] bool immortal = false;
 
     public void Kill()
     {
+        if (immortal) return;
         Instantiate(deadPrefab, transform.position, transform.rotation);
         gameObject.SetActive(false);
     }
