149111a9118f2904a794951c9604380eb86eb2e8
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-08-04 00:23:28 +1200

    Progress on boss

--- Prison Break/Assets/BossLogic.cs
+++ Prison Break/Assets/BossLogic.cs
@@ -1,28 +1,32 @@
 using Pathfinding;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using System;
 
 public class BossLogic : MonoBehaviour
 {
+    private bool isCoolingDown = false;
+
     [Header("New Config")]
     [SerializeField] private float phase1MaxHealth, phase2MaxHealth;
-    [SerializeField] private float p1Attack1Chance, p1Attack2Chance, p1Attack3Chance, p2Attack1Chance, p2Attack2Chance;
     [SerializeField] private CircleCollider2D playerTooCloseRadius;
+    [SerializeField] private Collider2D punchRange;
     [SerializeField] private float knifeSpeed;
     [SerializeField] private float chaseSpeed;
     [SerializeField] private int dynamiteCount;
     [SerializeField] private float turnSpeed;
+    [SerializeField] private float minCooldownTime, maxCooldownTime;
 
     [Header("Assignables")]
     [SerializeField] private AIDestinationSetter aiDestinationSetter;
+    [SerializeField] private Transform playerTransform;
     [SerializeField] private AIPath aiPath;
-    private Transform playerTransform;
+    [SerializeField] private BossMeleeCollision bossMeleeCollision;
     private float phase1CurrentHealth, phase2CurrentHealth; 
     private float maxHealth => phase1MaxHealth + phase2MaxHealth;
     private float currentHealth => phase1CurrentHealth + phase2CurrentHealth;
     private bool isPhase2 => phase1CurrentHealth <= 0f;
+    private Attacks currentAttack;
+    private bool isAttacking;
     private enum BossState
     {
         Attacking, Waiting, Dead
@@ -38,20 +42,89 @@ public class BossLogic : MonoBehaviour
         phase2CurrentHealth = phase2MaxHealth;
         LeanTween.init();
         LeanTween.reset();
-        playerTransform = GameObject.FindGameObjectWithTag("Player").transform;
         aiPath.maxSpeed = chaseSpeed;
     }
 
-    private void ChooseAttack()
+    private void Update()
     {
-        /*if (isPhase2)
+        if (isAttacking)
         {
-            int x = Random.Range(1, p2Attack1Chance + p2Attack2Chance);
-            Attacks attack = 
+            switch (currentAttack)
+            {
+                case Attacks.Punch:
+                    Punch();
+                    break;
+                case Attacks.KnifeThrow:
+                    KnifeThrow();
+                    break;
+                case Attacks.KnifeThrow2:
+                    KnifeThrow2();
+                    break;
+                case Attacks.DynamiteThrow:
+                    DynamiteThrow();
+                    break;
+            }
+        }
+        else if (!isCoolingDown) DoAttack();
+    }
+
+    private Attacks ChooseAttack()
+    {
+        if (isPhase2)
+        {
+            int x = UnityEngine.Random.Range(0, 3);
+            print(x);
+            if (x == 0) return Attacks.Punch;
+            else if (x == 1) return Attacks.KnifeThrow2;
+            else return Attacks.DynamiteThrow;
         }
         else
         {
-            
-        }*/
+            int x = UnityEngine.Random.Range(0, 2);
+            print(x);
+            if (x == 0) return Attacks.Punch;
+            else return Attacks.KnifeThrow;
+        }
+    }
+
+    private void DoAttack()
+    {
+        isAttacking = true;
+        currentAttack = ChooseAttack();
+
+    }
+    private void Punch()
+    {
+        aiPath.canMove = true;
+        aiDestinationSetter.target = playerTransform;
+        if (bossMeleeCollision.isTouchingPlayer)
+        {
+            print("Punch");
+            aiDestinationSetter.target = null;
+            aiPath.canMove = false;
+            StartCoroutine(AttackCooldown());
+            isAttacking = false;
+        }
+    }
+    private void KnifeThrow()
+    {
+        print("Knife throw");
+        StartCoroutine(AttackCooldown());
+        isAttacking = false;
+    }
+    private void KnifeThrow2()
+    {
+        print("Knife throw 2");
+    }
+    private void DynamiteThrow() 
+    {
+        print("Dynamite throw");
+    }
+
+    private IEnumerator AttackCooldown()
+    {
+        isCoolingDown = true;
+        yield return new WaitForSeconds(Random.Range(minCooldownTime, maxCooldownTime));
+        isCoolingDown = false;
     }
 }
---
+++ Prison Break/Assets/BossMeleeCollision.cs
@@ -0,0 +1,18 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossMeleeCollision : MonoBehaviour
+{
+    public bool isTouchingPlayer = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") isTouchingPlayer = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player") isTouchingPlayer = false;
+    }
+}
