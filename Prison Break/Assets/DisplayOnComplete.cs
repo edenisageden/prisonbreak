c752bb06df318613e756973ccf57a99e91403c67
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-08-30 17:00:29 +1200

    k

--- Prison Break/Assets/DisplayOnComplete.cs
+++ Prison Break/Assets/DisplayOnComplete.cs
@@ -7,10 +7,11 @@ public class DisplayOnComplete : MonoBehaviour
     [SerializeField] private NextLevelBox nextLevelBox;
     [SerializeField] private Animator animator;
 
-    private void Update()
+    private void LateUpdate()
     {
         if (nextLevelBox.isCompleteFully)
         {
+            Time.timeScale = 1.0f;
             animator.SetBool("isComplete", true);
         }
     }
