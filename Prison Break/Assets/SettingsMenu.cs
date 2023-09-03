be14a61c2599f35866d352cb42e7caa4d2b0e8d7
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-09-04 11:32:12 +1200

    c

--- Prison Break/Assets/SettingsMenu.cs
+++ Prison Break/Assets/SettingsMenu.cs
@@ -8,12 +8,24 @@ public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
     [SerializeField] private Slider audioSlider;
+    [SerializeField] private Toggle postProcessingToggle, screenshakeToggle;
 
     private void Start()
     {
         float value;
         audioMixer.GetFloat("Volume", out value);
         audioSlider.value = value;
+    }
+
+    public void SetPostProcessing(bool isOn)
+    {
+        if (isOn) PlayerPrefs.SetInt("PostProcessing", 1);
+        else PlayerPrefs.SetInt("PostProcessing", 0);
+    }
+    public void SetScreenshake(bool isOn)
+    {
+        if (isOn) PlayerPrefs.SetInt("Screenshake", 1);
+        else PlayerPrefs.SetInt("Screenshake", 0);
     }
 
     public void SetVolume(float volume)
