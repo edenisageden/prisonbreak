ec6284cd720ffff056d924096f71f064b921dd5d
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-09-03 17:23:51 +1200

    c

--- Prison Break/Assets/UIAudioMiddleMan.cs
+++ Prison Break/Assets/UIAudioMiddleMan.cs
@@ -8,7 +8,7 @@ public class UIAudioMiddleMan : MonoBehaviour
 
     private void Awake()
     {
-        uIAudio = FindAnyObjectByType<UIAudio>();
+        uIAudio = FindObjectOfType<UIAudio>();
     }
     public void PlayUI1()
     {
