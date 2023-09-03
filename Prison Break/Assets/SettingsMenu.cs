1e1e3aa3ffd4d5f12312a20417bdc3af4d8a40b6
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-09-04 11:27:50 +1200

    c

--- Prison Break/Assets/SettingsMenu.cs
+++ Prison Break/Assets/SettingsMenu.cs
@@ -2,10 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
 
 public class SettingsMenu : MonoBehaviour
 {
     public AudioMixer audioMixer;
+    [SerializeField] private Slider audioSlider;
+
+    private void Start()
+    {
+        float value;
+        audioMixer.GetFloat("Volume", out value);
+        audioSlider.value = value;
+    }
 
     public void SetVolume(float volume)
     {
