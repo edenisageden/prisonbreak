f79ce7dd7f779965fbe6b6cdae31cd693cb24a3f
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-09-03 19:27:55 +1200

    c

--- Prison Break/Assets/CursorManager.cs
+++ Prison Break/Assets/CursorManager.cs
@@ -6,9 +6,16 @@ public class CursorManager : MonoBehaviour
 {
     public bool cursorIsCrossHair;
     [SerializeField] private Texture2D mouseCursorTexture, crossHairTexture;
+    [SerializeField] private NextLevelBox nextLevelBox;
+    [SerializeField] private GameMenuManager gameMenuManager;
 
     private void Update()
     {
+        if (nextLevelBox.isCompleteFully) cursorIsCrossHair = false;
+        else cursorIsCrossHair = true;
+        if (gameMenuManager.isPaused) cursorIsCrossHair = false;
+        else cursorIsCrossHair = true;
+
         if (cursorIsCrossHair)
         {
             Cursor.SetCursor(crossHairTexture, new Vector2(10, 10), CursorMode.Auto);
