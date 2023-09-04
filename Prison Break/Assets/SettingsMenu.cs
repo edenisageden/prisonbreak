a68efde14c131d1c464511ee36e8797d2d913c8c
ZoomerStudios <121367656+ZoomerStudios@users.noreply.github.com>
2023-09-04 14:45:07 +1200

    c

--- Prison Break/Assets/SettingsMenu.cs
+++ Prison Break/Assets/SettingsMenu.cs
@@ -16,22 +16,27 @@ public class SettingsMenu : MonoBehaviour
         audioMixer.GetFloat("Volume", out value);
         audioSlider.value = value;
 
-        if (PlayerPrefs.GetInt("Screenshake") == 1) screenshakeToggle.isOn = true;
+        InitializeToggles();
+    }
+
+    private void InitializeToggles()
+    {
+        if (PlayerPrefs.GetInt("Screenshake") == 0) screenshakeToggle.isOn = true;
         else screenshakeToggle.isOn = false;
 
-        if (PlayerPrefs.GetInt("PostProcessing") == 1) postProcessingToggle.isOn = true;
+        if (PlayerPrefs.GetInt("PostProcessing") == 0) postProcessingToggle.isOn = true;
         else postProcessingToggle.isOn = false;
     }
 
     public void SetPostProcessing(bool isOn)
     {
-        if (isOn) PlayerPrefs.SetInt("PostProcessing", 1);
-        else PlayerPrefs.SetInt("PostProcessing", 0);
+        if (isOn) PlayerPrefs.SetInt("PostProcessing", 0);
+        else PlayerPrefs.SetInt("PostProcessing", 1);
     }
     public void SetScreenshake(bool isOn)
     {
-        if (isOn) PlayerPrefs.SetInt("Screenshake", 1);
-        else PlayerPrefs.SetInt("Screenshake", 0);
+        if (isOn) PlayerPrefs.SetInt("Screenshake", 0);
+        else PlayerPrefs.SetInt("Screenshake", 1);
     }
 
     public void SetVolume(float volume)
